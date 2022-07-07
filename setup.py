4fb395eb69d701b32150c85e2577cc2862b3ecbc
Unax <sefsg>
2022-07-07 18:03:50 +0200

    test

--- setup.py
+++ setup.py
@@ -2,14 +2,13 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='python-package-example',
+    name='MyINsuranceScriptPipeline',
     version='0.2',
     packages=find_packages(exclude=['tests*']),
     license='MIT',
     description='An example python package',
-    long_description=open('README.txt').read(),
     install_requires=['numpy'],
-    url='https://github.com/BillMills/python-package-example',
-    author='Bill Mills',
+    url='https://github.com/UnaxMendi/MyInsuranceScript.git',
+    author='Unax',
     author_email='myemail@example.com'
 )
